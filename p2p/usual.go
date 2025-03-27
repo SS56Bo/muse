057c50474b2e88b6e9a44c303ee63c448668b78f
SS56Bo <sohamsarkar560@gmail.com>
2025-03-28 02:11:53 +0530

    Initial Commit

---
+++ p2p/usual.go
@@ -0,0 +1,9 @@
+package p2p
+
+// transport will handle the communications between
+// nodes present in the network.
+type transport struct { }
+
+
+// Peer will handle anything related to remote nodes
+type peer struct { }
